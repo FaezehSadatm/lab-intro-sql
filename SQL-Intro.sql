b7642b478f054a22fa43b631a1c2598c4fb0671d
FaezehSadat <faezehsadatm@gmail.com>
2022-11-05 15:53:43 +0100

    SQL-Intro

---
+++ SQL-Intro.sql
@@ -0,0 +1,8 @@
+select * from actor;
+select * from film;
+select * from customer;
+select title from film;
+select name as language from language;
+select count(address) as address_count from address;
+select count(staff_id) as staff_count from staff;
+select first_name from staff;
