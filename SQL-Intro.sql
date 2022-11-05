4c6b3da86f72e0775aec5556f5dd56f9e011646c
FaezehSadat <faezehsadatm@gmail.com>
2022-11-05 22:04:13 +0100

    SQL-Intro

--- SQL-Intro.sql
+++ SQL-Intro.sql
@@ -1,8 +1,22 @@
-select * from actor;
-select * from film;
-select * from customer;
-select title from film;
-select name as language from language;
-select count(address) as address_count from address;
-select count(staff_id) as staff_count from staff;
-select first_name from staff;
+#1.Use sakila database.
+use sakila;
+
+#2.Get all the data from tables actor, film and customer.
+select * from `actor`;
+select * from `film`;
+select * from `customer`;
+
+#3.Get film titles.
+select `title` from `film`;
+
+#4.Get unique list of film languages under the alias language.
+select distinct(`name`) as `language` from `language`;
+
+#5.1 Find out how many stores does the company have?
+select count(`store_id`) as `store_count` from `store`;
+
+#5.2 Find out how many employees staff does the company have?
+select count(`staff_id`) as `staff_count` from `staff`;
+
+#5.3 Return a list of employee first names only?
+select `first_name` from `staff`;
